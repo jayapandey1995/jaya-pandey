a614d1bc210983276cfc6423dbb1abae3a66eabd
Dev (alt) <dev@maropost10sMini.localdomain>
2017-11-08 12:56:24 +0530

    message

--- exe7.rb
+++ exe7.rb
@@ -1,15 +1,22 @@
-formatter = "%{first} %{second} %{third} %{fourth}"
+puts "Mary had a little lamb."
+puts "Its fleece was white as #{'snow'}."
+puts "And everywhere that Mary went."
+puts "." * 10  # what'd that do?
 
-puts formatter % {first: 1, second: 2, third: 3, fourth: 4}
-puts formatter % {first: "one", second: "two", third: "three", fourth: "four"}
-puts formatter % {first: true, second: false, third: true, fourth: false}
-puts formatter % {first: formatter, second: formatter, third: formatter, fourth: formatter}
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
 
-puts formatter % {
-  first: "I had this thing.",
-  second: "That you could type up right.",
-  third: "But it didn't sing.",
-  fourth: "So I said goodnight."
-}
-
+# watch that print vs. puts on this line what's it do?
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
 
